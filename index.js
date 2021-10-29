0fa82ee445425c04c3fa920b656c77fd8d8d52a4
mehedih20 <mehedih20@gmail.com>
2021-10-30 00:29:10 +0600

    get data done

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient } = require("mongodb");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 // --------------------------------
@@ -8,7 +9,27 @@ app.use(cors());
 app.use(express.json());
 // --------------------------------
 
-async function run() {}
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ds1tl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db("SparkyTours");
+    const toursCollection = database.collection("tours");
+
+    // Get all tours
+    app.get("/tours", async (req, res) => {
+      const result = await toursCollection.find({}).toArray();
+      res.send(result);
+    });
+  } finally {
+    // await client.close()
+  }
+}
 
 run().catch(console.dir);
 
